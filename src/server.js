2783467aeecd1e8a453991a8f347b36569184765
Igor Trimailov <igor@igors-mbp.home>
2019-10-31 20:21:48 +0000

    add localhost to cors whitelist

--- src/server.js
+++ src/server.js
@@ -15,12 +15,22 @@ const validateUser = require('./api/validators/users')
 
 const app = express()
 
-app.use(
-  cors({
-    origin: 'https://gentle-brushlands-92013.herokuapp.com',
-    optionsSuccessStatus: 200,
-  }),
-)
+// move to config?
+var whitelist = [
+  'https://gentle-brushlands-92013.herokuapp.com',
+  'http://localhost:3000',
+]
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+app.use(cors(corsOptions))
 
 app.set('secretKey', config.secretKey)
 
